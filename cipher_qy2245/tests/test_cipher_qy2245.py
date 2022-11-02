f6334457c98638061dee822f0333c11cd63bc7b0
fredayuan77 <qy2245@columbia.edu>
2022-11-02 11:07:38 -0400

    add tests

--- cipher_qy2245/tests/test_cipher_qy2245.py
+++ cipher_qy2245/tests/test_cipher_qy2245.py
@@ -1,1 +1,8 @@
 from cipher_qy2245 import cipher_qy2245
+
+import pytest
+def test_cipher():
+    expected = 'bqqmf'
+    actual = cipher_qy2245.cipher('apple',1,True)
+    assert actual == expected, 'cipher not working'
+test_cipher()
